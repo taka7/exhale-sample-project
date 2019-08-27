9964cdfdf179d7758c473264f4e5be7cd6ed5270
Takayoshi Nobuoka <takayoshi.nobuoka@tri-ad.global>
2019-08-27 13:16:34 +0900

    add sample example and conf

---
+++ doc/index.rst
@@ -0,0 +1,9 @@
+Welcome to example
+==================
+
+.. toctree::
+   :maxdepth: 2
+   :caption: Contents:
+
+   api/library_root.rst
+
---
+++ include/test_directory/example_file.h
@@ -0,0 +1,14 @@
+#ifndef EXAMPLE_H_
+#define EXAMPLE_H_
+
+/**
+ * @file
+ */
+class Example {
+  /**
+   * @brief constructor
+   */
+  Example() = default;
+};
+
+#endif // EXAMPLE_H_
